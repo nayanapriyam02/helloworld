70d767d6235a1efe48b46e687145891ca1b085f4
nayanapriyam02 <102804297+nayanapriyam02@users.noreply.github.com>
2022-04-01 18:28:13 +0530

    print hello world
    printing hello world on java

---
+++ Demo_helloworld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo_helloworld;
+
+/**
+ *
+ * @author nayan
+ */
+public class Demo_helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+    
+}
